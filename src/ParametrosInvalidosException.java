9015f1e5928ad520a78535f22b78c672b69172d7
Fernando Henrique <fernandoh.class@gmail.com>
2023-09-25 18:45:37 -0300

    Initial commit

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends  Throwable{
+        public ParametrosInvalidosException (){
+            System.out.println (" ATENÇAO !!O Parametro dois precisa se maior que o Parametro um !!");
+        }
+
+}
